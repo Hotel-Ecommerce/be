9bb78fd9c25481848c1d12932ba944d92fcfd6a9
phatlt17420 <phatlt17420@gmail.com>
2025-07-15 00:19:30 +0700

    update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 // Middleware
 app.use(express.json()); // Phân tích cú pháp body JSON
 app.use(express.urlencoded({ extended: true })); // Phân tích cú pháp dữ liệu URL-encoded
-app.use(cors()); // Kích hoạt CORS cho tất cả các nguồn (cho mục đích phát triển)
+app.use(cors()); // Kích hoạt CORS 
 
 // Phục vụ các file tĩnh (hình ảnh đã upload)
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
@@ -28,7 +28,7 @@ const bookingRoutes = require('./routes/bookingRoutes');
 const employeeRoutes = require('./routes/employeeRoutes');
 const statisticRoutes = require('./routes/statisticRoutes');
 
-// Gắn các rout
+// Gắn các route
 app.use('/auth', authRoutes);
 app.use('/customers', customerRoutes);
 app.use('/rooms', roomRoutes);
@@ -41,7 +41,7 @@ app.use((err, req, res, next) => {
     console.error(err.stack); // Log lỗi stack để debug
     res.status(err.statusCode || 500).json({
         message: err.message || 'Something went wrong!',
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack // Chỉ hiển thị stack trong môi trường phát triển
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack // Chỉ hiển thị stack trong dev
     });
 });
 
