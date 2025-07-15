8100345e15f1e45ddd13284ce4875bf7a512eb8b
phatlt17420 <phatlt17420@gmail.com>
2025-07-16 00:15:02 +0700

    changePassword

--- controllers/authController.js
+++ controllers/authController.js
@@ -89,3 +89,36 @@ exports.login = asyncHandler(async (req, res) => {
 exports.signout = (req, res) => {
     res.status(200).json({ message: 'Đăng xuất thành công' });
 };
+
+
+//  Thay đổi mật khẩu
+
+exports.changePassword = asyncHandler(async (req, res) => {
+    const { currentPassword, newPassword } = req.body;
+
+    // Lấy thông tin người dùng từ req.user (được gán bởi middleware 'protect')
+    let user;
+    if (req.user.role === 'Customer') {
+        user = await Customer.findById(req.user._id);
+    } else { // dành cho Manager hoặc Admin tìm trong Employee
+        user = await Employee.findById(req.user._id);
+    }
+
+    if (!user) {
+        res.status(404);
+        throw new Error('Không tìm thấy user trong database.');
+    }
+
+    // 1. Kiểm tra mật khẩu hiện tại nhập đúng chưa
+    if (!(await user.matchPassword(currentPassword))) {
+        res.status(400);
+        throw new Error('Mật khẩu hiện tại không đúng.');
+    }
+
+    // 2. Cập nhật mật khẩu mới
+    // Mongoose pre-save hook sẽ tự động hash newPassword trước khi lưu
+    user.password = newPassword;
+    await user.save();
+
+    res.status(200).json({ message: 'Mật khẩu đã được thay đổi thành công.' });
+});
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { signupCustomer, login, signout } = require('../controllers/authController');
+const { signupCustomer, login, signout, changePassword } = require('../controllers/authController');
+const protect = require('../middleware/authMiddleware')
 const router = express.Router();
 
 router.post('/signup', signupCustomer);
 router.post('/login', login);
 router.post('/signout', signout);
+router.post('/changePassword', protect, changePassword);
 
 module.exports = router;
