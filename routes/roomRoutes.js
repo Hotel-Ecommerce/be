dbdecdc4ba558a8adc6d6305afb960d8ea51d2bb
dphuong2103 <midouz.project@gmail.com>
2025-07-17 16:14:00 +0700

    add available room list

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -6,7 +6,8 @@ const {
     addRoom,
     getRoomById,
     updateRoom,
-    deleteRoom
+    deleteRoom,
+    getAvailableRooms
 } = require('../controllers/roomController');
 const protect = require('../middleware/authMiddleware');
 const authorize = require('../middleware/permissionMiddleware');
@@ -26,6 +27,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 router.get('/list', getRooms); // Công khai, không cần xác thực
+router.get('/list/available', getAvailableRooms); // Công khai, không cần xác thực
 router.post('/add', protect, authorize(['Manager', 'Admin']), upload.array('images', 5), addRoom); // Tối đa 5 hình ảnh
 router.get('/:id', getRoomById); // Công khai, không cần xác thực
 router.post('/update', protect, authorize(['Manager', 'Admin']), upload.array('images', 5), updateRoom);
