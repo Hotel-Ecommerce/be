dec41377b29d522bd59ef31baaf997de3e778145
phatlt17420 <phatlt17420@gmail.com>
2025-07-27 01:04:44 +0700

    a

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -21,21 +21,21 @@ router.post('/add', protect, authorize(['Manager', 'Admin', 'Customer']), addBoo
 router.get('/:id', protect, authorize(['Manager', 'Admin', 'Customer']), getBookingById); // lấy thông tin đạt phòng qua Id
 router.post('/update', protect, authorize(['Manager', 'Admin']), updateBooking);
 router.post('/delete', protect, authorize(['Manager', 'Admin', 'Customer']), deleteBooking);  //Customer xóa booking của chính họ, Manager và Admin xóa bất kỳ booking nào.
-router.put('/markBookingPaid/:id', protect, authorize(['Manager', 'Admin', 'Customer']), markBookingPaid);
+router.put('/markBookingPaid/:id', protect, authorize(['Manager', 'Admin', 'Customer']), markBookingPaid); // đánh dấu đã thanh toan1
 
 //Route Yêu cầu Thay đổi/Hủy Booking
 
 // Gửi yêu cầu thay đổi booking (Customer)
 router.post('/bookingChangeRequests/update', protect, authorize('Customer'), requestBookingChange);
 
-// Gửi yêu cầu hủy booking (cho Customer)
+// Gửi yêu cầu hủy booking (Customer)
 router.post('/bookingChangeRequests/cancel', protect, authorize('Customer'), requestBookingCancellation);
 // Lấy tất cả các yêu cầu thay đổi/hủy booking (cho Admin/Manager)
 router.get('/bookingChangeRequests', protect, authorize(['Manager', 'Admin']), getBookingChangeRequests);
-// Admin phê duyệt yêu cầu thay đổi/hủy booking
-router.put('/bookingChangeRequests/:id/approve', protect, authorize('Admin'), approveBookingChangeRequest);
-// Admin từ chối yêu cầu thay đổi/hủy booking
-router.put('/bookingChangeRequests/:id/disapprove', protect, authorize('Admin'), disapproveBookingChangeRequest);
+// Admin, Manager phê duyệt yêu cầu thay đổi/hủy booking
+router.put('/bookingChangeRequests/:id/approve', protect, authorize(['Manager', 'Admin']), approveBookingChangeRequest);
+// Admin, Manager từ chối yêu cầu thay đổi/hủy booking
+router.put('/bookingChangeRequests/:id/disapprove', protect, authorize(['Manager', 'Admin']), disapproveBookingChangeRequest);
 
 
 
