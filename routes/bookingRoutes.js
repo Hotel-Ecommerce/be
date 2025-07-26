fc9776b59a0f55cad119297d91542b1cc29f0dc9
phatlt17420 <phatlt17420@gmail.com>
2025-07-27 00:42:18 +0700

    update tính năng ChangeBooking request

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -5,18 +5,38 @@ import {
     getBookingById,
     updateBooking,
     deleteBooking,
-    markBookingPaid
+    markBookingPaid,
+    requestBookingChange,
+    requestBookingCancellation,
+    getBookingChangeRequests,
+    approveBookingChangeRequest,
+    disapproveBookingChangeRequest,
 } from '../controllers/bookingController.js';
 import protect from '../middleware/authMiddleware.js';
 import authorize from '../middleware/permissionMiddleware.js';
 const router = express.Router();
 
 router.get('/list', protect, authorize(['Manager', 'Admin', 'Customer']), getBookings);
-router.post('/add', protect, authorize(['Manager', 'Admin', 'Customer']), addBooking);
-router.get('/:id', protect, authorize(['Manager', 'Admin', 'Customer']), getBookingById);
+router.post('/add', protect, authorize(['Manager', 'Admin', 'Customer']), addBooking); // thêm đặt phòng mới
+router.get('/:id', protect, authorize(['Manager', 'Admin', 'Customer']), getBookingById); // lấy thông tin đạt phòng qua Id
 router.post('/update', protect, authorize(['Manager', 'Admin']), updateBooking);
-router.post('/delete', protect, authorize(['Manager']), deleteBooking); // Chỉ Manager có thể xóa booking
+router.post('/delete', protect, authorize(['Manager', 'Admin', 'Customer']), deleteBooking);  //Customer xóa booking của chính họ, Manager và Admin xóa bất kỳ booking nào.
 router.put('/markBookingPaid/:id', protect, authorize(['Manager', 'Admin', 'Customer']), markBookingPaid);
+
+//Route Yêu cầu Thay đổi/Hủy Booking
+
+// Gửi yêu cầu thay đổi booking (Customer)
+router.post('/bookingChangeRequests/update', protect, authorize('Customer'), requestBookingChange);
+
+// Gửi yêu cầu hủy booking (cho Customer)
+router.post('/bookingChangeRequests/cancel', protect, authorize('Customer'), requestBookingCancellation);
+// Lấy tất cả các yêu cầu thay đổi/hủy booking (cho Admin/Manager)
+router.get('/bookingChangeRequests', protect, authorize(['Manager', 'Admin']), getBookingChangeRequests);
+// Admin phê duyệt yêu cầu thay đổi/hủy booking
+router.put('/bookingChangeRequests/:id/approve', protect, authorize('Admin'), approveBookingChangeRequest);
+// Admin từ chối yêu cầu thay đổi/hủy booking
+router.put('/bookingChangeRequests/:id/disapprove', protect, authorize('Admin'), disapproveBookingChangeRequest);
+
 
 
 export default router;
